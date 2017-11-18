e84b1fc4c2d7d9b56ccf25b7ffd2439389074c2d
akshaybapat04 <akshaybapat04@gmail.com>
2017-11-17 18:07:48 -0800

    Working code
    Working, just slow

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -7,16 +7,10 @@ using namespace std;
 
 #define iter 1000;
 
-bool is_solved(vector <int> v){
+bool is_solved(vector <int> &v){
 	for(int i=0;i<v.size();i++){
 		for(int j=i+1;j<v.size();j++){
-			if(v[i] == v[j]){
-				return 0;
-			}
-			else if(v[i]-v[j] == i-j){
-				return 0;
-			}
-			else if(v[i]-v[j] == j-i){
+			if((v[i] == v[j])||(v[i]-v[j] == i-j)||(v[i]-v[j] == j-i)){
 				return 0;
 			}
 		}
@@ -24,7 +18,7 @@ bool is_solved(vector <int> v){
 	return 1;
 }
 
-int count_conflicts(vector <int> v){
+int count_conflicts(vector <int> &v){
 	int conflicts=0;
 	for(int i=0;i<v.size();i++){
 		for(int j=i+1;j<v.size();j++){
@@ -37,19 +31,23 @@ int count_conflicts(vector <int> v){
 }
 
 int randval(int x){
+//	if(x==0){
+//		
+//	}
 	return rand()%x;
 }
 
-int findbestval(vector <int> v, int loc){
+int findbestval(vector <int> &v, int loc){
 	int i,n = v.size();
+	vector <int> v1 = v;
 	vector <int> all_conflicts;
 	for(i=0;i<n;i++){
-		v[loc] = i;
-		all_conflicts.push_back(count_conflicts(v));
+		v1[loc] = i;
+		all_conflicts.push_back(count_conflicts(v1));
 	}
 	int minval = all_conflicts[0];
 	for(i=1;i<n;i++){
-		if(minval<all_conflicts[i]){
+		if(minval>all_conflicts[i]){
 			minval = all_conflicts[i];
 		}
 	}
@@ -59,7 +57,9 @@ int findbestval(vector <int> v, int loc){
 			choosefromthesevals.push_back(i);
 		}
 	}
-	return choosefromthesevals[randval(choosefromthesevals.size())];
+	int bestvalchosen = choosefromthesevals[randval(choosefromthesevals.size())];
+//	cout<<"Value chosen="<<bestvalchosen<<" for loc="<<loc<<endl;
+	return bestvalchosen;
 }
 
 void min_conflicts(vector <int> &v){
@@ -72,15 +72,7 @@ void min_conflicts(vector <int> &v){
 				continue;
 			}
 			else{
-				if(v[i] == v[j]){
-					num_conf++;
-					break;
-				}
-				else if(v[i]-v[j] == i-j){
-					num_conf++;
-					break;
-				}
-				else if(v[i]-v[j] == j-i){
+				if((v[i] == v[j])||(v[i]-v[j] == i-j)||(v[i]-v[j] == j-i)){
 					num_conf++;
 					break;
 				}
@@ -89,16 +81,25 @@ void min_conflicts(vector <int> &v){
 		if(num_conf>0){
 			conflicts.push_back(i);
 		}
-		
 	}
 
-	// Print all conflicting colums
-	for(int i=0;i<conflicts.size();i++){
-		cout<<conflicts[i]<<" ";
-	}
-	cout<<endl;	
+//	if(conflicts.size()==0){
+//		cout<<"Yaay"<<endl;
+//		for(int i=0;i<n;i++){
+//			cout<<v[i]<<" ";
+//		}
+//		cout<<endl;
+//		return;
+//	}
 	
+	// Print all conflicting colums
+//	for(int i=0;i<conflicts.size();i++){
+//		cout<<conflicts[i]<<" ";
+//	}
+//	cout<<endl;
+
 	int rand_loc = conflicts[randval(conflicts.size())];
+//	cout<<"column selected="<<rand_loc<<endl;
 	//Pass by ref instead
 	v[rand_loc] = findbestval(v,rand_loc);
 }
@@ -106,34 +107,35 @@ void min_conflicts(vector <int> &v){
 void solve(int n){
 	int i,count=iter;
 	vector <int> v;
-//	for(i=0;i<n;i++){
-//		v.push_back(randval(n));
-//	}
-	
-	cout<<"Initial State:";
 	for(i=0;i<n;i++){
-		cout<<v[i]<<" ";
+		v.push_back(randval(n));
 	}
-	cout<<endl;
-	cout<<"==============="<<endl;
+	v[0]=3;v[1]=0;v[2]=3;v[3]=1;
+//	cout<<"Initial State:";
+//	for(i=0;i<n;i++){
+//		cout<<v[i]<<" ";
+//	}
+//	cout<<endl;
+//	cout<<"==============="<<endl;
 	
 	while(!is_solved(v) && count){
+//		cout<<"For iteration "<<count<<endl;
 		min_conflicts(v);
-		cout<<"For iteration "<<count<<endl;
-//		if(count%10==0){
-//			cout<<count_conflicts(v)<<" ";
-//		}
+		if(is_solved(v)){
+			cout<<"Solved!";
+			break;
+		}
+		if(count%20==0){
+			cout<<count<<": "<<count_conflicts(v)<<" "<<endl;
+		}
 		count--;
-	}
-	if(is_solved(v)){
-		cout<<"Solved!";
 	}
 }
 
 int main(){
 	int n;
 	srand (time(NULL));
-	n = 4;
+	n = 500;
 	solve(n);
 	return 0;
 }	
